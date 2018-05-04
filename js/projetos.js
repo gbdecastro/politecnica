3ee26ee35cdec8a4abf714ad59fa1b671db41c84
gbdecastro <gabriel_castr@outlook.com.brgit config --global user.email gabriel_castr@outlook.com.brgit config --global user.email gabriel_castr@outlook.com.br>
2018-05-04 12:01:56 -0400

    Projetos fix

--- js/projetos.js
+++ js/projetos.js
@@ -180,10 +180,9 @@ new Vue({
       projeto.id_empresa = $('#id_empresa').val();
       projeto.id_grupo = $('#id_grupo').val();
       projeto.cs_status = $('#cs_status').val();
-      projeto.id_projeto = $('#id_projeto').val();
-      
-      console.log(projeto.id_projeto);
 
+      console.log(projeto.id_projeto);
+      
       projeto.tx_projeto = projeto.tx_projeto.toUpperCase();
 
       if(projeto.tx_projeto == '' || projeto.id_projeto == ''){
