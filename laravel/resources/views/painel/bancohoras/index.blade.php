e513ad180da438c5490054aefc2f14848e839402
Javellan <javellan01@outlook.jp>
2019-01-04 15:39:44 -0200

    quadro dias uteis <html>

--- laravel/resources/views/painel/bancohoras/index.blade.php
+++ laravel/resources/views/painel/bancohoras/index.blade.php
@@ -82,7 +82,232 @@
     		</div>
     	</div>
     </div>
+
+	<div class="row">
+    	<!-- BOX DE Seleção de Dias Uteis -->
+    	<div class="col-md-12">
+    		<div class="box box-poli">
+    			<div class="box-header with-border">
+	    			<h3 class="box-title">
+	    				Quadro de Dias Úteis por Mês
+	    			</h3>
+	    			<div class="box-tools pull-right">
+	    				<button class="btn btn-box-toll" type="button" data-widget="collapse">
+	    					<i class="fa fa-minus"></i>
+	    				</button>
+	    				<button class="btn btn-box-toll" type="button" data-widget="remove">
+	    					<i class="fa fa-times"></i>
+	    				</button>    				
+	    			</div>
+    			</div>
+    			<div class="box-body">
+    				<div class="table-responsive">
+    					<table id="tableDiasUteis" class="table no-margin">
+    						
+    						<tbody>
+    							
+    							<tr>
+    								<td>Janeiro: 
+									<div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5">19.5</option>
+										<option value="20">20</option>
+										<option value="20.5" selected>20.5</option>   
+										<option value="21">21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22">22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div>
+
+									</td>
+
+                                    <td>Fevereiro: 
+									<div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5" selected>19.5</option>
+										<option value="20">20</option>
+										<option value="20.5">20.5</option>   
+										<option value="21">21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22">22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div></td>                              
+
+                                    <td>Março: 
+									<div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5">19.5</option>
+										<option value="20">20</option>
+										<option value="20.5">20.5</option>   
+										<option value="21" selected>21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22">22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div></td>    
+
+                                    <td>Abril: 
+									<div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5">19.5</option>
+										<option value="20">20</option>
+										<option value="20.5">20.5</option>   
+										<option value="21">21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22" selected>22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div></td>                              
+                                    
+									<td>Maio: 
+									<div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5">19.5</option>
+										<option value="20">20</option>
+										<option value="20.5">20.5</option>   
+										<option value="21" selected>21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22" >22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div></td>
+
+                                    <td>Junho: 
+									<div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5">19.5</option>
+										<option value="20" selected>20</option>
+										<option value="20.5">20.5</option>   
+										<option value="21">21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22">22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div></td>
+    							</tr>    							
+    							<tr>
+									<td>Julho: 
+									<div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5">19.5</option>
+										<option value="20" selected>20</option>
+										<option value="20.5">20.5</option>   
+										<option value="21">21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22">22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div></td>
+
+									<td>Agosto:<div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5">19.5</option>
+										<option value="20">20</option>
+										<option value="20.5">20.5</option>   
+										<option value="21">21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22">22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div> </td>                              
+
+									<td>Setembro: <div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5">19.5</option>
+										<option value="20">20</option>
+										<option value="20.5">20.5</option>   
+										<option value="21">21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22">22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div></td>    
+
+									<td>Outubro: <div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5">19.5</option>
+										<option value="20">20</option>
+										<option value="20.5">20.5</option>   
+										<option value="21">21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22">22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div></td>                              
+
+									<td>Novembro:<div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5">19.5</option>
+										<option value="20">20</option>
+										<option value="20.5">20.5</option>   
+										<option value="21">21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22">22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div> </td>
+
+									<td>Dezembro: <div class="form-group">
+									<select class="form-control select2-native" id="edit_dias_jan" name="dias_uteis" required>
+										<option value="18.5">18.5</option>
+										<option value="19">19</option>
+										<option value="19.5">19.5</option>
+										<option value="20">20</option>
+										<option value="20.5">20.5</option>   
+										<option value="21">21</option>   
+										<option value="21.5">21.5</option>   
+										<option value="22">22</option>   
+										<option value="22.5">22.5</option>   
+										<option value="23">23</option>   
+									</select>
+									<div></td>                            
+                              
+    							</tr>    
+
+    						</tbody>
+    					</table>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
 @endsection
+
 @section('js')
 <script>
 	$(function(){
