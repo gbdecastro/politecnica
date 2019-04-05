88c898d2a8cda9ab8cea79fb303eedc36a7b2dd0
Gabriel de Castro <gabriel_castr@outlook.com.br>
2019-04-05 13:08:11 -0400

    Nome do Funcionario no Modal de Calendario da tela de Colaborador

--- js/calendario-funcionario.js
+++ js/calendario-funcionario.js
@@ -197,8 +197,7 @@ function init(){
 $('#modal_calendario').on('show.bs.modal', function (event) {
     var el = $(event.relatedTarget)
     id_funcionario = el.data('id_funcionario')
-    $('.modalt-tile').html('Calendário: '+el.data('tx_funcionario'))
-
+    $('.modalt-tile').html('Calendário'+el.data('tx_funcionario'))
     init();
 });
 
